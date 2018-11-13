01de34fc362b656f0065efabdef176e9f93d5fec
Flávio Henrique da Silva Veloso <flavio.primecontrol@luizalabs.com>
2018-11-13 18:59:25 -0200

    First Commit

---
+++ src/br/com/devmedia/aprendendojava/dominio/Colecionador.java
@@ -0,0 +1,7 @@
+package br.com.devmedia.aprendendojava.dominio;
+
+public class Colecionador {
+	
+	
+
+}
---
+++ src/br/com/devmedia/aprendendojava/dominio/Item.java
@@ -0,0 +1,21 @@
+package br.com.devmedia.aprendendojava.dominio;
+
+public class Item {
+	
+	private String nome;
+	private int anoCriacao;
+	
+	public Item(String nome, int anoCriacao) {
+		this.nome = nome;
+		this.anoCriacao = anoCriacao;
+	}
+	
+	public String getNome() {return nome;}
+	public void setNome(String nome) {this.nome = nome;}
+	
+	public int getAnoCriacao() {return anoCriacao;}
+	public void setAnoCriacao(int anoCriacao) {this.anoCriacao = anoCriacao;}
+	
+	
+	
+}
