5f7fae5eec20ab8c9003409d3de1f300d4a3af8c
Flávio Henrique da Silva Veloso <flavio.primecontrol@luizalabs.com>
2018-11-14 18:11:08 -0200

    classe colecionador

--- src/br/com/devmedia/aprendendojava/dominio/Colecionador.java
+++ src/br/com/devmedia/aprendendojava/dominio/Colecionador.java
@@ -1,7 +1,16 @@
 package br.com.devmedia.aprendendojava.dominio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Colecionador {
 	
+	private String nome;
+	private List<Item> colecao;
 	
+	public Colecionador(String nome) {
+		this.nome = nome;
+		colecao = new ArrayList<Item>();
+	}
 
 }
